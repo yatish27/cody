5b5a82cf3b91ca31b9a62a0e6465b7aeaa20d948
Chris Fung <aergonaut@gmail.com>
2017-12-27 01:35:34 -0800

    Webpacker upgrade (#102)
    * bundle builds successfully with updated webpack
    
    * ignore graphql module for flow
    
    * remove extra webpack files

---
+++ config/webpack/environment.js
@@ -0,0 +1,23 @@
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "CommonsChunkVendor",
+  new webpack.optimize.CommonsChunkPlugin({
+    name: "vendor",
+    minChunks: module => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf("node_modules") !== -1;
+    }
+  })
+);
+
+environment.plugins.append(
+  "CommonsChunkManifest",
+  new webpack.optimize.CommonsChunkPlugin({
+    name: "manifest",
+    minChunks: Infinity
+  })
+);
+
+module.exports = environment;

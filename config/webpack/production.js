f7f2720f32daa455e57ef608eecba7541400bb2a
Chris Fung <aergonaut@gmail.com>
2018-01-03 14:01:11 -0800

    use babel-minify in production instead of uglifyjs

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,6 @@
 const environment = require("./environment");
 
+// Use babel-minify in production instead of UglifyJS
+environment.plugins.delete('UglifyJs');
+
 module.exports = environment.toWebpackConfig();

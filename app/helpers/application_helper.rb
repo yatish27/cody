6cdb822fa3520e28cc81ca8b252ce67370433ccb
Chris Fung <aergonaut@gmail.com>
2018-07-03 14:21:18 -0700

    fix mailer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def pull_request_url(pull_request)
-    "https://github.com/#{pull_request.repository}/pull/#{pull_request.number}"
+    "https://github.com/#{pull_request.repository.full_name}/pull/#{pull_request.number}"
   end
 end
